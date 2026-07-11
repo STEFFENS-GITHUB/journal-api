d8bb64495fb1674b0cd0e0a25c804ac3b8ba3dc0
Steffen <Redacted>
2026-07-10 20:25:57 -0500

    Fixed integration tests to work with Token Bearer Auth.

--- app/tests/integration/test_journal.py
+++ app/tests/integration/test_journal.py
@@ -5,10 +5,20 @@ async def create_test_journal(client, auth_headers):
     response = await client.post("/api/journal/create",
                             json={"title":"Post Test Title", "body":"Post Test Body"},
                             headers=auth_headers)
-    assert response.status_code == 201
     journal = response.json()
     yield journal["id"]
+    await client.delete(f"/api/journal/{journal['id']}", headers=auth_headers)
 
+
+async def test_create_journal(client, auth_headers):
+    response = await client.post("/api/journal/create",
+                            json={"title":"Post Test Title", "body":"Post Test Body"},
+                            headers=auth_headers)
+    assert response.status_code == 201
+    journal = response.json()
+    assert journal["title"] == "Post Test Title"
+    assert journal["body"] == "Post Test Body"
+    await client.delete(f"/api/journal/{journal['id']}", headers=auth_headers)
 
 async def test_delete_journal(client, auth_headers, create_test_journal):
     response = await client.delete(f"/api/journal/{create_test_journal}", headers=auth_headers)
@@ -41,8 +51,8 @@ async def test_update_journal(client, auth_headers, create_test_journal):
     assert journal["title"] == "Patch Test Title"
     assert journal["body"] == "Patch Test Body"
 
-async def test_get_journal(client, create_test_journal):
-    response = await client.get(f"/api/journal/{create_test_journal}")
+async def test_get_journal(client, auth_headers, create_test_journal):
+    response = await client.get(f"/api/journal/{create_test_journal}", headers=auth_headers)
     assert response.status_code == 200
     journal = response.json()
     assert journal["title"]
--- app/tests/integration/test_user.py
+++ app/tests/integration/test_user.py
@@ -1,15 +1,17 @@
 import pytest
 
 @pytest.fixture
-async def create_test_user(client, auth_headers):
+async def create_test_user(client):
     response = await client.post("/api/user/create", json={"username":"test_user", "password":"123"})
-    assert response.status_code == 201
     user = response.json()
-    yield user["id"]
-    await client.delete(f"/api/user/{user['id']}", headers=auth_headers) # Uses default user creds to delete, rather then test user, will need changing
+    response = await client.post("/login", data={"username":"test_user", "password":"123"})
+    headers = {"Authorization": f"Bearer {response.json()['access_token']}"}
+    yield user["id"], headers
+    await client.delete(f"/api/user/{user['id']}", headers=headers)
 
 async def test_get_user(client, create_test_user):
-    response = await client.get(f"/api/user/{create_test_user}")
+    user_id, headers = create_test_user
+    response = await client.get(f"/api/user/{user_id}", headers=headers)
     assert response.status_code == 200
     user = response.json()
     assert user["username"]
